7bcca968ced7a814b09e1cb439468711a0fe0c9b
Uday Varmora <varmorauday1045@gmail.com>
2025-12-09 17:29:50 +0530

    added ecosystem.config.js file

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,22 @@
+require("dotenv").config({ path: "/etc/nodeapp/.env.generated" });
+
+module.exports = {
+    apps: [
+        {
+            name: 'mern-mysql-backend',
+            script: 'server.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'development',
+                PORT: 5000,
+            },
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: 5000,
+            },
+        },
+    ],
+};
